18831da3a0a15f6fc60662c0d7f0da772e692d5a
mohammadmiras <mohammad.miras.1377@gmail.com>
2023-09-06 13:23:04 +0330

    Script Push

--- AdminApi/Program.cs
+++ AdminApi/Program.cs
@@ -1,1 +1,43 @@
-Application.Run();
+// Application.Run();
+
+// var conn = new NpgsqlConnection("Server=database;Database=blog;User Id=root;Password=1234");
+// conn.Open();
+// NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(" select * from pg_namespace ", conn);
+// var table = new DataTable();
+// adapter.Fill(table);
+// Logger.LogError(table.Rows.Count.ToString());
+// conn.Close();
+
+// SqlConnection conn = new SqlConnection(connString);
+// SqlCommand cmd = new SqlCommand(query, conn);
+// conn.Open();
+
+// // create data adapter
+// SqlDataAdapter da = new SqlDataAdapter(cmd);
+// // this will query your database and return the result to your datatable
+// da.Fill(dataTable);
+// conn.Close();
+// da.Dispose();
+
+//   using(NpgsqlConnection connection = new NpgsqlConnection()) {  
+//             connection.ConnectionString = "Server=database;Database=blog;User Id=root;Password=1234";  
+
+//             connection.Open();  
+//             NpgsqlCommand cmd = new NpgsqlCommand();  
+//             cmd.UnknownResultTypeList = new[] { false, true };
+//             cmd.Connection = connection;  
+//             cmd.CommandText = "select * from pg_namespace";  
+//             cmd.CommandType = CommandType.Text;  
+//             NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);  
+//             DataTable dt = new DataTable();  
+//             da.Fill(dt);  
+//             cmd.Dispose();  
+//             connection.Close();  
+//         }
+NpgsqlConnection connecton1 = new NpgsqlConnection("Server=database;Database=blog;User Id=root;Password=1234");
+connecton1.Open();
+NpgsqlCommand cmd = new NpgsqlCommand("select * from pg_namespace",connecton1);
+var db = new DataTable();
+var dr  = cmd.ExecuteReader();
+db.Load(dr);
+
